52dc55bbb382b6e1f673bbdcdc65cecfad64fd65
matosjoaops <matosjoaops@protonmail.com>
2023-03-12 13:03:10 +0000

    Setup a more complex test project for hermit

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Hello World!')
+import axios from "axios";
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  axios
+    .get("https://jsonplaceholder.typicode.com/todos/1")
+    .then((resp) => {
+      res.send(resp.data);
+    })
+    .catch((resp) => {
+      res.send("Error!");
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
